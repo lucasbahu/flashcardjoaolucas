d0d972b5b55f84e7951e04fd52d2770e3feb87fd
lucasbahu <140765610+lucasbahu@users.noreply.github.com>
2024-11-12 22:05:36 -0300

    Add files via upload

--- flashcardjoaolucas/perguntas.js
+++ flashcardjoaolucas/perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é um retificador?',
+    'Os retificadores são dispositivos elétricos amplamente utilizados na indústria para converter corrente alternada'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a função do carburador?',
+    'A finalidade do carburador é preparar a mistura de ar-combustível que tornará possível a combustão e fará o motor funcionar. Em outras palavras, a peça combina o ar quente com o combustível disponível no reservatório do automóvel, permitindo que nos cilindros do motor aconteça uma combustão segura e eficiente.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é um pistão?',
+    'Como componente do motor de combustão, o pistão é responsável por converter a energia liberada durante o processo de combustão em trabalho mecânico e transferi-la como torque para o virabrequim, através do pino de pistão e da biela. O pistão se move para cima e para baixo no cilindro quando o motor está trabalhando.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se fala "Como você está?" em inglês?',
-    'how are you? (Como vc está?)'
+    'Oque é um Cabeçote?',
+    'Essa peça é indispensável para o funcionamento do motor, gerenciando a admissão de ar e o escape do mesmo através de válvulas. Responsável pela transmissão térmica e de combustível, é claro que o cabeçote não poderia ser feito com um material qualquer, afinal ele deve ser resistente'
 )
